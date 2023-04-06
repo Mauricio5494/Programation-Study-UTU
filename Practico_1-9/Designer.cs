59879c6bfd69ad7664d7359933658eafd27ce947
Mauricio5494 <mauriciogorilluberas@gmail.com>
2023-04-05 23:16:56 -0300

    Create Designer.cs

---
+++ Practico_1-9/Designer.cs
@@ -0,0 +1,270 @@
+namespace Practico_1_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonP15_1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP15_2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP12_1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP15_3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP12_2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP18 = new System.Windows.Forms.RadioButton();
+            this.radioButtonP9 = new System.Windows.Forms.RadioButton();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButtonATP = new System.Windows.Forms.RadioButton();
+            this.labSentenciaRes = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edad";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Black;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.labSentenciaRes);
+            this.groupBox1.Controls.Add(this.radioButtonATP);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioButtonP9);
+            this.groupBox1.Controls.Add(this.radioButtonP18);
+            this.groupBox1.Controls.Add(this.radioButtonP12_2);
+            this.groupBox1.Controls.Add(this.radioButtonP15_3);
+            this.groupBox1.Controls.Add(this.radioButtonP12_1);
+            this.groupBox1.Controls.Add(this.radioButtonP15_2);
+            this.groupBox1.Controls.Add(this.radioButtonP15_1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(193, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 301);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Películas";
+            // 
+            // radioButtonP15_1
+            // 
+            this.radioButtonP15_1.AutoSize = true;
+            this.radioButtonP15_1.Location = new System.Drawing.Point(9, 38);
+            this.radioButtonP15_1.Name = "radioButtonP15_1";
+            this.radioButtonP15_1.Size = new System.Drawing.Size(105, 17);
+            this.radioButtonP15_1.TabIndex = 0;
+            this.radioButtonP15_1.TabStop = true;
+            this.radioButtonP15_1.Text = "El Teléfono | +15";
+            this.radioButtonP15_1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP15_2
+            // 
+            this.radioButtonP15_2.AutoSize = true;
+            this.radioButtonP15_2.Location = new System.Drawing.Point(9, 58);
+            this.radioButtonP15_2.Name = "radioButtonP15_2";
+            this.radioButtonP15_2.Size = new System.Drawing.Size(161, 17);
+            this.radioButtonP15_2.TabIndex = 1;
+            this.radioButtonP15_2.TabStop = true;
+            this.radioButtonP15_2.Text = "¿Que le pasó a Lunes? | +15";
+            this.radioButtonP15_2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP12_1
+            // 
+            this.radioButtonP12_1.AutoSize = true;
+            this.radioButtonP12_1.Location = new System.Drawing.Point(9, 81);
+            this.radioButtonP12_1.Name = "radioButtonP12_1";
+            this.radioButtonP12_1.Size = new System.Drawing.Size(140, 17);
+            this.radioButtonP12_1.TabIndex = 2;
+            this.radioButtonP12_1.TabStop = true;
+            this.radioButtonP12_1.Text = "Una voz silenciosa | +12";
+            this.radioButtonP12_1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP15_3
+            // 
+            this.radioButtonP15_3.AutoSize = true;
+            this.radioButtonP15_3.Location = new System.Drawing.Point(9, 104);
+            this.radioButtonP15_3.Name = "radioButtonP15_3";
+            this.radioButtonP15_3.Size = new System.Drawing.Size(197, 17);
+            this.radioButtonP15_3.TabIndex = 3;
+            this.radioButtonP15_3.TabStop = true;
+            this.radioButtonP15_3.Text = "Mobile Suit Gundam Hathaway | +15";
+            this.radioButtonP15_3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP12_2
+            // 
+            this.radioButtonP12_2.AutoSize = true;
+            this.radioButtonP12_2.Location = new System.Drawing.Point(9, 127);
+            this.radioButtonP12_2.Name = "radioButtonP12_2";
+            this.radioButtonP12_2.Size = new System.Drawing.Size(222, 17);
+            this.radioButtonP12_2.TabIndex = 4;
+            this.radioButtonP12_2.TabStop = true;
+            this.radioButtonP12_2.Text = "Shiki Oriori - Sabores de la Juventúd | +12";
+            this.radioButtonP12_2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP18
+            // 
+            this.radioButtonP18.AutoSize = true;
+            this.radioButtonP18.Location = new System.Drawing.Point(9, 150);
+            this.radioButtonP18.Name = "radioButtonP18";
+            this.radioButtonP18.Size = new System.Drawing.Size(159, 17);
+            this.radioButtonP18.TabIndex = 5;
+            this.radioButtonP18.TabStop = true;
+            this.radioButtonP18.Text = "Ghost in the shell 1995 | +18";
+            this.radioButtonP18.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonP9
+            // 
+            this.radioButtonP9.AutoSize = true;
+            this.radioButtonP9.Location = new System.Drawing.Point(9, 173);
+            this.radioButtonP9.Name = "radioButtonP9";
+            this.radioButtonP9.Size = new System.Drawing.Size(128, 17);
+            this.radioButtonP9.TabIndex = 6;
+            this.radioButtonP9.TabStop = true;
+            this.radioButtonP9.Text = "Heroes Modestos | +9";
+            this.radioButtonP9.UseVisualStyleBackColor = true;
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.BackColor = System.Drawing.Color.Black;
+            this.txbEdad.ForeColor = System.Drawing.Color.White;
+            this.txbEdad.Location = new System.Drawing.Point(97, 37);
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(29, 20);
+            this.txbEdad.TabIndex = 2;
+            this.txbEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbEdad_KeyPress);
+            this.txbEdad.Validating += new System.ComponentModel.CancelEventHandler(this.txbEdad_Validating);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Location = new System.Drawing.Point(62, 69);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Aviso: Todas estas películas están en Netflix para ver.";
+            // 
+            // radioButtonATP
+            // 
+            this.radioButtonATP.AutoSize = true;
+            this.radioButtonATP.Location = new System.Drawing.Point(9, 196);
+            this.radioButtonATP.Name = "radioButtonATP";
+            this.radioButtonATP.Size = new System.Drawing.Size(160, 17);
+            this.radioButtonATP.TabIndex = 7;
+            this.radioButtonATP.TabStop = true;
+            this.radioButtonATP.Text = "Barbie Mermaid Power | ATP";
+            this.radioButtonATP.UseVisualStyleBackColor = true;
+            // 
+            // labSentenciaRes
+            // 
+            this.labSentenciaRes.AutoSize = true;
+            this.labSentenciaRes.Location = new System.Drawing.Point(95, 233);
+            this.labSentenciaRes.Name = "labSentenciaRes";
+            this.labSentenciaRes.Size = new System.Drawing.Size(16, 13);
+            this.labSentenciaRes.TabIndex = 8;
+            this.labSentenciaRes.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sentencia:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(249, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Productos \'El Gori\'";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(621, 397);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txbEdad);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Clasificación por edades para ver películas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonP9;
+        private System.Windows.Forms.RadioButton radioButtonP18;
+        private System.Windows.Forms.RadioButton radioButtonP12_2;
+        private System.Windows.Forms.RadioButton radioButtonP15_3;
+        private System.Windows.Forms.RadioButton radioButtonP12_1;
+        private System.Windows.Forms.RadioButton radioButtonP15_2;
+        private System.Windows.Forms.RadioButton radioButtonP15_1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labSentenciaRes;
+        private System.Windows.Forms.RadioButton radioButtonATP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
